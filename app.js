c19842f06d77b8ea33e3c066497dce58f110b97e
CamilaGuerrero00 <camilag.main@gmail.com>
2023-07-28 12:03:18 -0300

    quinta entrega

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const PORT = 3030;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',(req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get('/register',(req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
+app.get('/loguin',(req, res) => res.sendFile(path.join(__dirname, 'views', 'loguin.html')));
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'views', '404.html')));
 
 app.listen(PORT, () => console.log("server running in http://localhost:"+ PORT));
